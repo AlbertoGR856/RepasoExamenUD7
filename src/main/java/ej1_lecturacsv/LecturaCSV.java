96b30b47b3c0c373f802f8b50459a0ace0980b13
Alberto <albertogr578@gmail.com>
2022-05-10 19:40:49 +0200

    Ejercicio1 finalizado

--- src/main/java/ej1_lecturacsv/LecturaCSV.java
+++ src/main/java/ej1_lecturacsv/LecturaCSV.java
@@ -5,6 +5,12 @@
  */
 package ej1_lecturacsv;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  *
  * @author GR6
@@ -29,7 +35,56 @@ método es eficiente
 La creación de la estructura Map es correcta: se crea una estructura TreeMap y se
 mapean correctamente los campos del fichero en los pares clave-valor de la estructura
 0,4
-*/
+ */
 public class LecturaCSV {
-    
+
+    //Leer los ficheros de la carpeta “copias” e ir guardando los objetos en una lista de vehículos.
+    public static SortedMap<String, String> lecturaFicheroCSV(String fichero) {
+
+        //Variables para almacenar lo que se lee por Scanner
+        String linea;
+        String[] tokens;
+
+        SortedMap<String, String> inicialesDescripcion = new TreeMap<>();
+
+        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
+        // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
+        // las operaciones con el archivo. Hace uso del sistema UTF-8
+        //Se uso de la clase Scanner
+        //ESTRUCTURA PRINCIPAL DE LECTURA CON SCANNER
+        try ( Scanner datosFichero = new Scanner(new File(fichero), "ISO-8859-1")) {
+            // hasNextInt devuelve true mientras haya enteros por leer
+            datosFichero.nextLine();
+            while (datosFichero.hasNextLine()) {
+
+                //Se añade el contenido del fichero a la variable String linea
+                linea = datosFichero.nextLine();
+
+                //Se separa el contenio de la variable linea con : usando la funcion .split() 
+                tokens = linea.split(";");
+
+                Modulo modulo = new Modulo();
+
+                modulo.setInicial(tokens[0]);
+                modulo.setDescripcion(tokens[1]);
+
+                inicialesDescripcion.put(modulo.getInicial(), modulo.getDescripcion());
+            }
+
+            //Excepcion de tipo FileNotFoundException, en caso de que no exitiera el archivo a leer
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return inicialesDescripcion;
+    }
+
+    public static void main(String[] args) {
+
+        SortedMap<String, String> inicialesDescripcion = lecturaFicheroCSV("nombresModulos.csv");
+
+        //inicialesDescripcion.forEach(System.out::println);
+        inicialesDescripcion.forEach((clave, valor) -> System.out.println("Clave: " + clave + " -- Objeto: " + valor));
+
+    }
+
 }
