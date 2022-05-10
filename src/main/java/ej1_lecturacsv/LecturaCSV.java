6fb2c959aa1604b90c2a21bdfb810ddbd2687248
Alberto <albertogr578@gmail.com>
2022-05-10 19:01:18 +0200

    Añade la clase lecturaCSV al proyecto

---
+++ src/main/java/ej1_lecturacsv/LecturaCSV.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej1_lecturacsv;
+
+/**
+ *
+ * @author GR6
+ */
+/*
+Crea una clase, llamada LecturaCSV, que contenga un método que permita la lectura del fichero CSV de la raíz de tu
+proyecto. Este método recibe el nombre y ruta del fichero a leer y se encarga de devolver una estructura de tipo Map
+ordenada de forma que las iniciales de los módulos sean las claves y el nombre completo de cada módulo sean los
+valores asociados a dichas claves.
+Realiza una prueba de lectura en la clase Programa y muestra por consola el resultado obtenido. Presta atención al
+control de excepciones.
+
+La definición de la clase LecturaCSV, la firma del método de lectura y el tipo devuelto son
+correctos. No existen atributos innecesarios
+0,15
+
+La lectura del fichero se realiza correctamente: se utilizan las clases apropiadas, se
+recorren todos los registros y se liberan los recursos una vez finalizada la lectura. El
+método es eficiente
+0,45
+
+La creación de la estructura Map es correcta: se crea una estructura TreeMap y se
+mapean correctamente los campos del fichero en los pares clave-valor de la estructura
+0,4
+*/
+public class LecturaCSV {
+    
+}
