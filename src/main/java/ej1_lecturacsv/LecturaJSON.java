fe46b79d14c03349a97b383cf8965568d050489f
Alberto <albertogr578@gmail.com>
2022-05-10 20:14:01 +0200

    Ejercicio2 terminado

--- src/main/java/ej1_lecturacsv/LecturaJSON.java
+++ src/main/java/ej1_lecturacsv/LecturaJSON.java
@@ -5,6 +5,13 @@
  */
 package ej1_lecturacsv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author GR6
@@ -35,11 +42,41 @@ tratamiento de ficheros JSON
 La lectura del fichero se realiza correctamente, se utilizan las clases apropiadas y se
 guardan todos los registros en una lista. Se liberan los recursos una vez finalizada la
 lectura
-*/
+1
+ */
 public class LecturaJSON {
-    
-    public static void main(String[] args) {
-        
+
+    private static ArrayList<RegistroJSON> leerFicheroJSON(String fichero) throws IOException {
+
+        //Se crea y se inicializa un objeto de tipo  ObjectMapper 
+        ObjectMapper mapeador = new ObjectMapper();
+
+        //Permite a mapeador usar fechas según java time
+        mapeador.registerModule(new JavaTimeModule());
+
+        //Se crea la lista de objetos App y se procede a utilizar la funcion .readValue()
+        //Esta funcion se le pasa la ruta del archivo json a leer y el objeto de tipo mapeador (Incluyendo la clase App y la lista)
+        ArrayList<RegistroJSON> listaCalificaciones = mapeador.readValue(new File(fichero),
+                mapeador.getTypeFactory().constructCollectionType(ArrayList.class, RegistroJSON.class));
+
+//        //Se recorre la lista con un for-each y se imprime por consola
+//        System.out.println("---- Lista de Calificaciones ----");
+//
+//        for (RegistroJSON listaCalificacion : listaCalificaciones) {
+//
+//            System.out.println(listaCalificacion);
+//        }
+        return listaCalificaciones;
     }
-    
+
+    public static void main(String[] args) throws IOException {
+        
+         ArrayList<RegistroJSON> listaAlumnosCalificaciones = leerFicheroJSON("calificacionesGrupo.json");
+         
+          System.out.println("---- Lista de calificaciones de los alumnos ----");
+         listaAlumnosCalificaciones.forEach((listaCalificacion) -> {
+             System.out.println(listaCalificacion);
+        });
+
+    }
 }
